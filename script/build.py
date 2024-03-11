7c03e9b26ad33931bd9bc87662b4ad40aeba6247
Mona Lisa <limingyao6@hikvision.com>
2024-03-11 16:23:03 +0800

    skottie关闭

--- script/build.py
+++ script/build.py
@@ -37,7 +37,7 @@ def main():
     'skia_use_system_harfbuzz=false',
     'skia_pdf_subset_harfbuzz=true',
     'skia_use_system_icu=false',
-    'skia_enable_skottie=true',
+    'skia_enable_skottie=false',
     'skia_use_fontconfig=false',
     'skia_use_egl=true'
   ]
